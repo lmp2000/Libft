e503a6c7c42c77ce02cc1cb23c9a0458d2dea83e
lmp2000 <lmrpais@gmail.com>
2025-10-24 12:50:34 +0100

    commit

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: lude-jes <lude-jes@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 22:59:48 by lude-jes          #+#    #+#             */
-/*   Updated: 2025/10/24 00:27:29 by lude-jes         ###   ########.fr       */
+/*   Updated: 2025/10/24 12:42:33 by lude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	source = (unsigned char *)src;
 	destination = (unsigned char *)dest;
 	i = 0;
-
 	while (i < n)
 	{
 		destination[i] = source[i];
 		i++;
 	}
-
 	return (dest);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lude-jes <lude-jes@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 00:38:55 by lude-jes          #+#    #+#             */
-/*   Updated: 2025/10/24 00:50:24 by lude-jes         ###   ########.fr       */
+/*   Updated: 2025/10/24 12:48:34 by lude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,31 @@
 
 void	*ft_memove(void *dest, const void *src, size_t n)
 {
-		
+	unsigned char	*source;
+	unsigned char	*destination;
+	size_t			i;
+	
+	source = (unsigned char *)src;
+	destination = (unsigned char *)dest;
+	i = 0;
+	if (n == 0)
+		return (dest);
+	if (dest > src)
+	{
+		n--;
+		while (i <= n)
+		{
+			destination[n - i] = source[n - i];
+			i++;
+		}
+	}
+	else
+	{
+		while (i < n)
+		{
+			destination[i] = source[i];
+			i++;
+		}
+	}
+	return (dest);
 }
