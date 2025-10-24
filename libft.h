48271e43d6764c4e13716f1d589070e5b55328eb
lmp2000 <lmrpais@gmail.com>
2025-10-24 16:07:13 +0100

    commit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lude-jes <lude-jes@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 13:24:26 by lude-jes          #+#    #+#             */
-/*   Updated: 2025/10/24 14:11:54 by lude-jes         ###   ########.fr       */
+/*   Updated: 2025/10/24 16:06:06 by lude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memove(void *dest, const void *src, size_t n);
 int		ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-
+int		ft_toupper(int c);
+char	*ft_strchr(const char *s, int c);
 
 #endif
